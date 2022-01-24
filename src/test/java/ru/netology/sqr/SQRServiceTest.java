1e0529dfd13f520d7d7177bb86e116ba6e8d58ca
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-01-24 10:07:48 +0300

    added data.csv

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,12 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
     @ParameterizedTest
-    @CsvSource(value = {"square inside range,200,300,3",
-            "square under range,100,199,4",
-            "square over range,301,400,3",
-            "zero range,0,0,0",
-            "interval border square number,225,225,0",
-            "invalid range, 225,220,0"})
+//    @CsvSource(value = {"square inside range,200,300,3",
+//            "square under range,100,199,4",
+//            "square over range,301,400,3",
+//            "zero range,0,0,0",
+//            "interval border square number,225,225,0",
+//            "invalid range, 225,220,0"})
+
+    @CsvFileSource(resources = "/data.csv",delimiter = '|')
 
     public void shouldCount(String testName, int rangeStart, int rangeEnd, int expected) {
         SQRService service = new SQRService();
